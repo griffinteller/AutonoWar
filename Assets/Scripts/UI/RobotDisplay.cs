cc52ea803e40f0a67087f313ed96863c687958da
Griffin Teller <griffinteller@gmail.com>
2020-07-14 13:16:06 -0700

    Finished robot display dragging

--- Assets/Scripts/UI/RobotDisplay.cs
+++ Assets/Scripts/UI/RobotDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Building;
 using UnityEngine;
+using UnityEngine.UI;
 using Utility;
 
 namespace UI
@@ -12,9 +13,12 @@ namespace UI
         public DesignLoaderDisplay displayRobotPrefab;
         
         private const float Spacing = 8;
+        private const float MouseSensitivity = 0.015f;
+        private const float SmoothTime = 0.2f;
 
         private RobotStructure[] _structures;
-
+        private int _selectedIndex;
+        
         public static Vector3 GetRobotPositionAtIndex(int index)
         {
             return Vector3.right * Spacing * index;
@@ -30,6 +34,42 @@ namespace UI
             InstantiateRobots();
         }
 
+        public void Update()
+        {
+            KeyCheck();
+            
+            if (!Input.GetMouseButton(0))
+                SnapPosition();
+        }
+
+        private Vector3 _lastMousePos;
+        private void KeyCheck()
+        {
+            if (Input.GetMouseButtonDown(0))
+                _lastMousePos = Input.mousePosition;
+            
+            if (Input.GetMouseButton(0))
+            {
+                transform.Translate((Input.mousePosition - _lastMousePos).x * MouseSensitivity, 0, 0);
+                _lastMousePos = Input.mousePosition;
+            }
+        }
+
+        private Vector3 _velocity;
+        private void SnapPosition()
+        {
+            var t = transform;
+            var localPos = t.localPosition;
+            
+            var currentPosX = localPos.x;
+            var desiredPosX = Mathf.Clamp(Mathf.Round(currentPosX / Spacing) * Spacing, 
+                -(_structures.Length - 1) * Spacing, 0);
+            _selectedIndex = Mathf.RoundToInt(-desiredPosX / Spacing);
+            
+            var desiredPos = new Vector3(desiredPosX, localPos.y, localPos.z);
+            t.localPosition = Vector3.SmoothDamp(localPos, desiredPos, ref _velocity, SmoothTime);
+        }
+
         private void InstantiateRobots()
         {
             for (var i = 0; i < _structures.Length; i++)
@@ -39,7 +79,7 @@ namespace UI
                 var displayComponent = robotObj.GetComponent<DesignLoaderDisplay>();
 
                 displayComponent.structure = structure;
-                robotObj.transform.position = GetRobotPositionAtIndex(i);
+                robotObj.transform.localPosition = GetRobotPositionAtIndex(i);
             }
         }
     }
