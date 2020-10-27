c7e39a19e6788eccfb4e3e8ab9b52133478d0635
Griffin Teller <griffinteller@gmail.com>
2020-10-26 19:18:06 -0700

    Finished table generator and colored string

--- Assets/Scripts/UI/ColoredString.cs
+++ Assets/Scripts/UI/ColoredString.cs
@@ -1,126 +1,72 @@
-using System;
-using System.Collections.Generic;
-using UnityEditor.Graphs;
-using UnityEngine;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace UI
 {
-    public struct ColoredChar
-    {
-        public char rawChar;
-        public Color color;
-
-        public ColoredChar(char rawChar, Color? color = null)
-        {
-            this.rawChar = rawChar;
-            this.color = color ?? Color.black;
-        }
-    }
-    
     public class ColoredString
     {
-        private Dictionary<Color, int> _colorIndices; // we rename each color with an index, to save memory
-        private Dictionary<int, Color> _indexColors; // the reverse of _colorIndices, for color-lookup
-        private int[] _colorIndexArray; // the colors of each character, with the color index
-
+        public string RenderedString { get; }
         public string RawString { get; }
-        public int Length => RawString.Length;
+        public int RenderedLength => RenderedString.Length;
 
-        private ColoredString(string rawString, Dictionary<Color, int> colorIndices,
-            Dictionary<int, Color> indexColors, int[] colorIndexArray)
+        private ColoredString(string renderedString, string rawString)
+        {
+            RenderedString = renderedString;
+            RawString = rawString;
+        }
+
+        public ColoredString(string rawString)
         {
             RawString = rawString;
-            _colorIndices = colorIndices;
-            _indexColors = indexColors;
-            _colorIndexArray = colorIndexArray;
+            RenderedString = Regex.Replace(
+                rawString, @"(<color=#\d\d\d\d\d\d>)|(<\\color>)", "");
         }
 
-        public ColoredString(string str, Color? defaultColor = null, params (Color, (int, int))[] ranges)
+        public ColoredString(string renderedString, int defaultColor = 0, (int, int, int)[] colorAndRanges = null)
         {
-            RawString = str;
-            _colorIndices = new Dictionary<Color, int>
-            {
-                {defaultColor ?? Color.black, 0}
-            };
-            _indexColors = new Dictionary<int, Color>
-            {
-                {0, defaultColor ?? Color.black}
-            };
-            _colorIndexArray = new int[str.Length];
+            colorAndRanges = colorAndRanges ?? new (int, int, int)[0];
 
-            for (int rangeIndex = 0; rangeIndex < ranges.Length; rangeIndex++)
-            {
-                Color color = ranges[rangeIndex].Item1;
-                (int start, int end) = ranges[rangeIndex].Item2;
+            StringBuilder rawStringBuilder = new StringBuilder(renderedString);
 
-                int colorIndex;
-                if (_colorIndices.ContainsKey(color))
-                    colorIndex = _colorIndices[color];
-                else
-                {
-                    colorIndex = _colorIndices.Count;
-                    _colorIndices.Add(color, colorIndex);
-                    _indexColors.Add(colorIndex, color);
-                }
-                for (int charIndex = start; charIndex < end; charIndex++)
-                    _colorIndexArray[charIndex] = colorIndex;
+            foreach ((int color, int start, int end) in colorAndRanges) // add tags for given ranges
+            {
+                rawStringBuilder.Insert(start, "<color=#" + color.ToString("X6") + ">");
+                rawStringBuilder.Insert(end, "</color>");
             }
+
+            rawStringBuilder.Insert(0, "<color=#" + defaultColor.ToString("X6") + ">"); // add default color tags
+            rawStringBuilder.Append("</color>");
+
+            RenderedString = renderedString;
+            RawString = rawStringBuilder.ToString();
         }
 
-        public static ColoredString operator + (ColoredString str1, ColoredString str2)
+        public static ColoredString operator +(ColoredString str1, ColoredString str2)
         {
             return Concat(str1, str2);
         }
 
-        public static ColoredString Concat(ColoredString str1, ColoredString str2)
+        private static ColoredString Concat(ColoredString str1, ColoredString str2)
         {
-            ColoredString smallStr = str1.Length < str2.Length ? str1 : str2;
-            ColoredString largeStr = smallStr == str1 ? str2 : str1;
-
-            string newRawString = str1.RawString + str2.RawString;
-            Dictionary<Color, int> newColorIndices = largeStr._colorIndices;
-            Dictionary<int, Color> newIndexColors = largeStr._indexColors;
-            
-            // we need to make sure both string use the same color-index pairings
-            // its more efficient to switch the small string over to the "language" of the large string
-            int[] newSmallStrIndexArray = smallStr._colorIndexArray;
-            for (int i = 0; i < newSmallStrIndexArray.Length; i++)
-            {
-                // switch to the new pairing
-                newSmallStrIndexArray[i] = newColorIndices[smallStr._indexColors[smallStr._colorIndexArray[i]]];
-            }
-
-            int[] newColorIndexArray = new int[newRawString.Length];
-            if (smallStr == str1)
-            {
-                newSmallStrIndexArray.CopyTo(newColorIndexArray, 0);
-                str2._colorIndexArray.CopyTo(newColorIndexArray, str1.Length);
-            }
-            else
-            {
-                str1._colorIndexArray.CopyTo(newColorIndexArray, 0);
-                newSmallStrIndexArray.CopyTo(newColorIndexArray, str1.Length);
-            }
-            
-            return new ColoredString(newRawString, newColorIndices, newIndexColors, newColorIndexArray);
+            return new ColoredString(
+                str1.RenderedString + str2.RenderedString,
+                str1.RawString + str2.RawString);
         }
 
-        public ColoredChar CharAt(int index)
+        public static ColoredString[] ColorStringArray(string[] arr, int color = 0)
         {
-            return new ColoredChar(
-                RawString.Substring(index, 1).ToCharArray()[0], 
-                _indexColors[_colorIndexArray[index]]);
+            ColoredString[] result = new ColoredString[arr.Length];
+            
+            for (int i = 0; i < arr.Length; i++)
+                result[i] = new ColoredString(arr[i], 0);
+            
+            return result;
         }
 
-        public ColoredString Substring(int startIndex, int length)
+        public override string ToString()
         {
-            string rawString = RawString.Substring(startIndex, length);
-            int[] newIndexArray = new int[length];
-            Array.Copy(_colorIndexArray, startIndex,
-                newIndexArray, 0, length);
-            
-            // TODO: Optimize memory by removing unused colors in new string from the dictionaries
-            return new ColoredString(rawString, _colorIndices, _indexColors, newIndexArray);
+            return RawString;
         }
     }
 }
--- Assets/Scripts/UI/TableTextGen.cs
+++ Assets/Scripts/UI/TableTextGen.cs
@@ -1,20 +1,108 @@
 using System.Collections.Generic;
+using System.Text;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace UI
 {
     public class TableTextGen
     {
-        [NotNull] public string[] columns;
-        [CanBeNull] public List<string[]> data;
+        public ColoredString[] columns;
+        public List<ColoredString[]> data;
 
-        public TableTextGen([NotNull] string[] columns, 
-            [CanBeNull] List<string[]> data = null)
+        public int width;
+        public int color;
+
+        public TableTextGen(
+            ColoredString[] columns, 
+            int color = 0, 
+            int width = -1,
+            [CanBeNull] List<ColoredString[]> data = null) // width of -1 means min width
         {
             this.columns = columns;
-            this.data = data;
+            this.data = data ?? new List<ColoredString[]>();
+            this.color = color;
+            this.width = width;
         }
-        
-        public 
+
+        public string Generate()
+        {
+            string minWidth = GenerateMinWidth();
+
+            return minWidth; // TODO: support custom widths
+        }
+
+        private string GenerateMinWidth()
+        {
+            ColoredString separator = new ColoredString("|", color);
+            
+            StringBuilder titleLineBuilder = new StringBuilder(); // build title line first
+            int[] maxColumnWidths = GetMaxComlumnWidths();
+            
+            for (int i = 0; i < columns.Length; i++)
+            {
+                ColoredString column = columns[i];
+                
+                int columnWidth = maxColumnWidths[i];
+                int delta = columnWidth - column.RenderedLength;
+                
+                titleLineBuilder.Append(" " 
+                                        + column 
+                                        + new string(' ', delta + 1) 
+                                        + (i + 1 == columns.Length ? "" : separator.RawString));
+            }
+
+            StringBuilder[] dataLineBuilders = new StringBuilder[data.Count];
+            for (int i = 0; i < dataLineBuilders.Length; i++) // build each line
+            {
+                dataLineBuilders[i] = new StringBuilder();
+                StringBuilder lineBuilder = dataLineBuilders[i];
+
+                for (int col = 0; col < data[i].Length; col++)
+                {
+                    ColoredString datum = data[i][col];
+                    int columnWidth = maxColumnWidths[col];
+                    Debug.Log(columnWidth);
+                    int delta = columnWidth - datum.RenderedLength;
+                    Debug.Log(delta);
+
+                    lineBuilder.Append(" " 
+                                       + datum 
+                                       + new string(' ', delta + 1) 
+                                       + (col + 1 == columns.Length ? "" : separator.RawString));
+                }
+
+                lineBuilder.Remove(lineBuilder.Length - 1, 1);
+            }
+
+            int totalWidth = 0;
+            foreach (int width in maxColumnWidths)
+                totalWidth += width + 3;
+            totalWidth -= 1; // because there is no "|" at the end
+
+            StringBuilder finalBuilder = new StringBuilder();
+            finalBuilder.AppendLine(titleLineBuilder.ToString());
+            finalBuilder.AppendLine(new string('-', totalWidth));
+            
+            foreach (StringBuilder lineBuilder in dataLineBuilders)
+                finalBuilder.AppendLine(lineBuilder.ToString());
+
+            return finalBuilder.ToString();
+        }
+
+        private int[] GetMaxComlumnWidths()
+        {
+            int[] maxColumnWidths = new int[columns.Length];
+            for (int col = 0; col < columns.Length; col++)
+                if (columns[col].RenderedLength > maxColumnWidths[col])
+                    maxColumnWidths[col] = columns[col].RenderedLength;
+            
+            for (int row = 0; row < data.Count; row++)
+                for (int col = 0; col < columns.Length; col++)
+                    if (data[row][col].RenderedLength > maxColumnWidths[col])
+                        maxColumnWidths[col] = data[row][col].RenderedLength;
+
+            return maxColumnWidths;
+        }
     }
 }
